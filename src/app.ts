8bef2a82ff478da0a02189a12358592a802af869
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2023-09-19 16:42:35 +0800

    popup and play

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,50 @@
 import Plyr from 'plyr'
+const Swal = require('sweetalert2')
 
 const app = () => {
-  console.log('monkey jumping on the bed.')
   const css = GM_getResourceText('plyrcss')
   GM_addStyle(css)
   kintone.events.on('app.record.detail.show', function (event) {
-    window.alert('记录详情页面已打开')
-    const htmlTemplate = `
-    <video id="player" playsinline controls>
-    <source src="/k/api/record/download.do/-/%E5%8D%97%E4%BA%AC%E5%BC%82%E8%A7%86%E7%95%8C%E7%A7%91%E6%8A%80%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8.mp4?app=3156&field=6673173&detectType=true&record=52&row=3158728&id=226753&hash=9a2b4620b35de324ee9553f4f07d96cc50b3a8b8&revision=3&.mp4" />
-    </video>`
-    const videoHtml = document.createElement('div')
-    videoHtml.innerHTML = htmlTemplate
-    // videoHtml.style.width = '800px'
-    document.body.appendChild(videoHtml)
-    const player = new Plyr('#player')
+    // find video element
+    const fileImageElements = document.querySelectorAll('li.file-image-container-gaia > a')
+    // loop fileImageElement
+    for (let i in fileImageElements) {
+      // prevent loop prototype
+      if (fileImageElements.hasOwnProperty(i)) {
+        console.log(fileImageElements[i])
+        const aEle = fileImageElements[i] as HTMLAnchorElement
+        console.log(aEle.href)
+        const regex = /\.mov|\.mp4$/gi
+        if (regex.test(aEle.href)) {
+          // play button
+          const aPlayButton = document.createElement('button')
+          aPlayButton.textContent = 'Play'
+          if (aEle.parentElement) {
+            aEle.parentElement.appendChild(aPlayButton)
+            aPlayButton.addEventListener('click', function () {
+              Swal.fire({
+                title: 'play it',
+                showConfirmButton: false,
+                showCancelButton: true,
+                cancelButtonText: 'close',
+                width: 640,
+                html: `<div><video id="player" playsinline controls><source src="${aEle.href}" /></video></div>`,
+                didOpen: () => {
+                  const player = new Plyr('#player')
+                },
+              })
+              // const htmlTemplate = `<video id="player" playsinline controls><source src="${aEle.href}" /></video>`
+              // const videoHtml = document.createElement('div')
+              // videoHtml.style.display
+              // videoHtml.innerHTML = htmlTemplate
+              // videoHtml.style.width = '800px'
+              // document.body.appendChild(videoHtml)
+            })
+          }
+        }
+      }
+    }
+
     return event
   })
 }
